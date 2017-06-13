d8f3d93df9cf6d97f5a3f5d443479f4ede9c089b
StarryPurp <starbloom128@gmail.com>
2017-06-13 20:58:09 +0900

    #5 Event select -1

--- app/src/main/java/bstar128/example/hansung/dbtest/MainActivity.java
+++ app/src/main/java/bstar128/example/hansung/dbtest/MainActivity.java
@@ -1,6 +1,7 @@
 package bstar128.example.hansung.dbtest;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
@@ -50,6 +51,19 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this,"저장완료!",Toast.LENGTH_LONG).show();//저장된거 알림
             }
         });
+        butSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqldb=mydb.getReadableDatabase();
+                String sql="select * from IdolTable";
+                Cursor cursor=sqldb.rawQuery(sql,null);
+                String names="Idol 이름"+"\r\n"+"==========="+"\r\n";
+                String counts="Idol 인원수"+"\r\n"+"==========="+"\r\n";
+                while(cursor.moveToNext()) {
+
+                }
+            }
+        });
     }
     class MyDBHelper extends SQLiteOpenHelper{
     //IdolDB라는 이름의 데이터베이스가 생성됨
@@ -60,13 +74,13 @@ public class MainActivity extends AppCompatActivity {
     //IdolDB하는 이름의 테이블 생성
         @Override
         public void onCreate(SQLiteDatabase db) {
-            String sql="create table IdolTable(idolname text not null primary key, Idolcount integer)";
+            String sql="create table IdolTable(Idolname text not null primary key, Idolcount integer)";
             db.execSQL(sql);
         }
 //이미 IdolDB이 존재하면 기존 테이블 삭제, 새로 테이블 만들때 호출
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            String sql="drop table ifexist idolTable";
+            String sql="drop table if exist idolTable";
             db.execSQL(sql);
             onCreate(db);
         }
