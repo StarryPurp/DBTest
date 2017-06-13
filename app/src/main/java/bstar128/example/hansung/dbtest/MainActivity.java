8cbbf807961c83095dee173f4f573652f3f5b85a
StarryPurp <starbloom128@gmail.com>
2017-06-13 20:33:19 +0900

    #3 Event 1-DB

--- app/src/main/java/bstar128/example/hansung/dbtest/MainActivity.java
+++ app/src/main/java/bstar128/example/hansung/dbtest/MainActivity.java
@@ -5,13 +5,40 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+    EditText editName,editCount,editResultName,editResultCount;
+    Button butInit, butInsert,butSelect;
+    MyDBHelper mydb;
+    SQLiteDatabase sqldb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editName=(EditText)findViewById(R.id.edit_group_name);
+        editCount=(EditText)findViewById(R.id.edit_group_count);
+        editResultName=(EditText)findViewById(R.id.result_name);
+        editResultCount=(EditText)findViewById(R.id.edit_group_count);
+        butInit=(Button)findViewById(R.id.but_insert);
+        butInsert=(Button)findViewById(R.id.but_insert);
+        butSelect=(Button)findViewById(R.id.but_select);
+
+        //DB 생성
+        mydb=new MyDBHelper(this);
+        //기존의 테이블이 존재하면 삭제하고 테이블을 새로 생성
+        butInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               sqldb= mydb.getWritableDatabase();
+                mydb.onUpgrade(sqldb,1,2);
+                sqldb.close();
+            }
+        });
     }
     class MyDBHelper extends SQLiteOpenHelper{
     //IdolDB라는 이름의 데이터베이스가 생성됨
